a46e31e6180156aa684da7913c3f88b3d3743a69
YUKIKO IKEDA <applelele@gmail.com>
2020-03-20 23:38:15 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,45 @@
-# code here!
+require "pry"
+
+class School
+  attr_writer :add_student, :grade, :sort
+  
+  def initialize(school_name)
+    @name = school_name
+    @roster = {}
+  end
+  
+  def name
+    @name
+  end
+  
+  def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+
+    if @roster.include? (@grade)
+        @roster[@grade] << @student_name
+    else
+        @roster[@grade] = []  #{9 => []}
+        @roster[@grade] << @student_name
+    end
+  end
+
+  def roster
+    @roster
+  end
+  
+  def grade(grade)
+    @grade = grade
+    @roster[@grade]
+  end
+  
+  def sort
+    sorted = {}
+    @roster.each do |grade, student_array|
+      sorted_student = student_array.sort
+      sorted[grade] = sorted_student
+    end
+    sorted
+  end
+end
+# school = School.new("Bayside High School")
